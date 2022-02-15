05e8fe229edc57a54cdf31745dc4830cb04d2a0a
Paul Niggli <58045354+Rommel25@users.noreply.github.com>
2022-02-15 22:00:35 +0100

    Nbr de cookies dans le shop

--- app/src/main/java/android/iut/td1/niggli/cookieclicker/MainActivity.java
+++ app/src/main/java/android/iut/td1/niggli/cookieclicker/MainActivity.java
@@ -1,21 +1,14 @@
 package android.iut.td1.niggli.cookieclicker;
 
-import android.annotation.SuppressLint;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.media.Image;
+import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         affichage = (TextView)findViewById(R.id.Affichage);
         ImageButton shop = (ImageButton)findViewById(R.id.btn_shop);
 
+
         if (savedInstanceState != null){
             compteur =savedInstanceState.getInt("Compteur");
             affichage.setText(Integer.toString(compteur));
@@ -41,7 +35,10 @@ public class MainActivity extends AppCompatActivity {
         shop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Shop.class));
+                Intent i = new Intent(MainActivity.this, Shop.class);
+                i.putExtra("Compteur",compteur);
+                startActivity(i);
+
             }
         });
 
--- app/src/main/java/android/iut/td1/niggli/cookieclicker/Shop.java
+++ app/src/main/java/android/iut/td1/niggli/cookieclicker/Shop.java
@@ -1,7 +1,9 @@
 package android.iut.td1.niggli.cookieclicker;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class Shop extends AppCompatActivity {
 
@@ -12,6 +14,11 @@ public class Shop extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
         getSupportActionBar().setTitle("Shop Dr. Sarrazinzin");
+        TextView affichage_cookies = (TextView)findViewById(R.id.Affichage_shop);
+        Intent intent = getIntent();
+        int compteur = intent.getIntExtra("Compteur",0);
+        affichage_cookies.setText(Integer.toString(compteur));
+
 
     }
 }
