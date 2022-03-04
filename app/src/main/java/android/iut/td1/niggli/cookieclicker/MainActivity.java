288159825e72455c20df036fdde85a9f41d89451
Paul Niggli <58045354+Rommel25@users.noreply.github.com>
2022-03-05 00:17:31 +0100

    Synchro prix + nbr + argent par click

--- app/src/main/java/android/iut/td1/niggli/cookieclicker/MainActivity.java
+++ app/src/main/java/android/iut/td1/niggli/cookieclicker/MainActivity.java
@@ -3,8 +3,8 @@ package android.iut.td1.niggli.cookieclicker;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
@@ -14,8 +14,12 @@ public class MainActivity extends AppCompatActivity {
 
     private int compteur = 0;
     private TextView affichage;
+    int nbr_cm = 0;
+    int prix_cookie_miteux = 10;
     private SharedPreferences myPreferences;
     private SharedPreferences.Editor editor;
+    //private Shop shop = new Shop();
+    final int REQUEST_CODE = 42;
 
 
     @Override
@@ -26,38 +30,33 @@ public class MainActivity extends AppCompatActivity {
         affichage = (TextView)findViewById(R.id.Affichage);
         ImageButton shop = (ImageButton)findViewById(R.id.btn_shop);
 
-        Intent intent = getIntent();
-        compteur = intent.getIntExtra("Compteur",0);
+        //Intent intent = getIntent();
+        //compteur = intent.getIntExtra("Compteur",0);
+        //nbr_cm = intent.getIntExtra("NBR_CM",0);
+        //prix_cookie_miteux = intent.getIntExtra("Prix",prix_cookie_miteux);
+
 
         if (savedInstanceState != null){
             compteur =savedInstanceState.getInt("Compteur");
             affichage.setText(Integer.toString(compteur));
         }
 
-        shop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, Shop.class);
-                i.putExtra("Compteur",compteur);
-                startActivity(i);
-
-            }
-        });
-
         affichage.setText(Integer.toString(compteur));
 
-        myPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        //myPreferences = getSharedPreferences("android.iut.td1.niggli.cookieclicker", Context.MODE_PRIVATE);
-        editor = myPreferences.edit();
 
-        editor.putInt("compteur",compteur);
-        editor.commit();
+    }
 
-        int repriseCompteur = myPreferences.getInt("compteur",0);
+    public void gotoShop(View v){
+        Intent i = new Intent(MainActivity.this, Shop.class);
+        i.putExtra("Compteur",compteur);
+        i.putExtra("nbr_cm",nbr_cm);
+        i.putExtra("prix_cm",prix_cookie_miteux);
+        startActivityForResult(i,REQUEST_CODE);
     }
 
     public void CountUP(View view){
-        compteur++;
+        int add = nbr_cm+1;
+        compteur+= add;
         affichage.setText(Integer.toString(compteur));
     }
 
@@ -71,5 +70,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         savedInstanceState.putInt("compteur",compteur);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK && data!=null) {
+            compteur = (int) data.getIntExtra("C_ret",0);
+            nbr_cm = (int) data.getIntExtra("nbr_cm",0);
+            prix_cookie_miteux = (int) data.getIntExtra("prix_cm",10);
+            affichage.setText(Integer.toString(compteur));
+        }
     }
 }
--- app/src/main/java/android/iut/td1/niggli/cookieclicker/Shop.java
+++ app/src/main/java/android/iut/td1/niggli/cookieclicker/Shop.java
@@ -2,53 +2,112 @@ package android.iut.td1.niggli.cookieclicker;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Shop extends AppCompatActivity {
 
     int compteur;
+    int nbrCM = 0;
     int prix_cookie_miteux = 10;
-    TextView aff_cookie_miteux, affichage_cookies;
+    TextView aff_prix_cookie_miteux, affichage_argent, aff_nbr_cm;
+    /*public int augmentation = 1;
 
+    public int getAugmentation() {
+        return augmentation;
+    }
+
+    public void setAugmentation(int augmentation) {
+        this.augmentation += augmentation;
+    }*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
         getSupportActionBar().setTitle("Shop du Dr. Sarrazinzin");
-        affichage_cookies = (TextView)findViewById(R.id.Affichage_shop);
+        affichage_argent = (TextView)findViewById(R.id.Affichage_shop);
+        aff_nbr_cm = (TextView)findViewById(R.id.nbr_miteux);
+        aff_prix_cookie_miteux = (TextView)findViewById(R.id.Affichage_prix_miteux);
         Intent intent = getIntent();
         compteur = intent.getIntExtra("Compteur",0);
-        affichage_cookies.setText(Integer.toString(compteur));
-        aff_cookie_miteux = (TextView)findViewById(R.id.Affichage_prix_miteux);
+        nbrCM = intent.getIntExtra("nbr_cm",0);
+        prix_cookie_miteux = intent.getIntExtra("prix_cm",10);
+
+
+        affichage_argent.setText(Integer.toString(compteur));
+        aff_nbr_cm.setText("Nbr:"+ nbrCM);
+        aff_prix_cookie_miteux.setText("Prix:"+ prix_cookie_miteux);
+
+
         ImageButton retour = (ImageButton)findViewById(R.id.Button_retour);
 
-        retour.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(Shop.this, MainActivity.class);
-                i.putExtra("Compteur",compteur);
-                startActivity(i);
 
-            }
-        });
+        if (savedInstanceState != null){
+            nbrCM =savedInstanceState.getInt("NBR_CM");
+            aff_nbr_cm.setText("Nbr:"+nbrCM);
+            prix_cookie_miteux = savedInstanceState.getInt("prix_cm");
+            aff_prix_cookie_miteux.setText("Prix:"+prix_cookie_miteux);
 
+        }
+
+
+
+
+
+    }
+
+    public void gotoRetour(View v){
+        affichage_argent = (TextView)findViewById(R.id.Affichage_shop);
+        String s_moula = affichage_argent.getText().toString();
+        int moula = Integer.parseInt(s_moula);
+        Intent i = new Intent();
+        i.putExtra("C_ret",moula);
+        i.putExtra("nbr_cm",nbrCM);
+        i.putExtra("prix_cm",prix_cookie_miteux);
+        setResult(RESULT_OK,i);
+        finish();
     }
 
     public void Achat_CM(View view){
-        compteur = compteur -10;
-        affichage_cookies.setText(Integer.toString(compteur));
-        prix_cookie_miteux += prix_cookie_miteux * 0.1;
-        aff_cookie_miteux.setText(Integer.toString(prix_cookie_miteux));
+        if (compteur > prix_cookie_miteux) {
+            compteur = compteur - prix_cookie_miteux;
+            affichage_argent.setText(Integer.toString(compteur));
+            nbrCM++;
+            prix_cookie_miteux = (int) ((int)(prix_cookie_miteux + nbrCM*2)*1.25);
+            aff_prix_cookie_miteux.setText("Prix:"+ prix_cookie_miteux);
+            aff_nbr_cm.setText("Nbr:"+ nbrCM);
+
+            //setAugmentation(1);
+        }
+        else {
+            Toast.makeText(Shop.this,
+                    "Pas assez de moulaga", Toast.LENGTH_SHORT).show();
+        }
     }
 
-    public boolean onOptionsItemSelected(MenuItem item){
+   /** public boolean onOptionsItemSelected(MenuItem item){
         Intent myIntent = new Intent(getApplicationContext(), MainActivity.class);
         startActivityForResult(myIntent, 0);
         return true;
+    }**/
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt("Prix_cookie_m",prix_cookie_miteux);
+        outState.putInt("NBR_CM",nbrCM);
+        super.onSaveInstanceState(outState);
     }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        savedInstanceState.putInt("Prix_cookie_m",prix_cookie_miteux);
+        savedInstanceState.putInt("NBR_CM",nbrCM);
+    }
+
 }
